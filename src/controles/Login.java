6fcf6b8d62556480ae76e9762855f15e78b8f1ed
104935 <104935@SALA411.proway.treina>
2018-05-28 09:55:15 -0300

    Trbalho

---
+++ src/controles/Login.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controles;
+
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author 104935
+ */
+public class Login {
+    
+    public static void autenticaUsuario(JTextField usuario, JPasswordField senha){
+        
+        
+        
+    }
+	
+}
---
+++ src/objetos/Listas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 104935
+ */
+public class Listas {
+    
+    public static ArrayList<Funcionario> equipe = new ArrayList();
+    public static ArrayList<Carro> carros = new ArrayList();
+    public static ArrayList<Venda> historicoVendas = new ArrayList();
+    
+}
